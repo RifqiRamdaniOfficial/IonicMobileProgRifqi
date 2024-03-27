c640c9e6d7d5349dbd1a8ac7ed8103adbc3001bd
RifqiRamdaniOfficial <rifqiramdani068@gmail.com>
2024-03-27 17:52:38 +0700

    Menambakan fitur konversi pada menu suhu

--- src/app/suhu/suhu.page.ts
+++ src/app/suhu/suhu.page.ts
@@ -5,11 +5,43 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './suhu.page.html',
   styleUrls: ['./suhu.page.scss'],
 })
-export class SuhuPage implements OnInit {
+export class SuhuPage {
+  inputTemperature: number = 0;
+  selectedFromUnit: string = '';
+  selectedToUnit: string = '';
+  result: number = 0;
 
-  constructor() { }
-
-  ngOnInit() {
+  convert() {
+    if (
+      this.selectedFromUnit === 'celsius' &&
+      this.selectedToUnit === 'fahrenheit'
+    ) {
+      this.result = (this.inputTemperature * 9) / 5 + 32;
+    } else if (
+      this.selectedFromUnit === 'celsius' &&
+      this.selectedToUnit === 'kelvin'
+    ) {
+      this.result = this.inputTemperature + 273.15;
+    } else if (
+      this.selectedFromUnit === 'fahrenheit' &&
+      this.selectedToUnit === 'celsius'
+    ) {
+      this.result = (this.inputTemperature - 32) * (5 / 9);
+    } else if (
+      this.selectedFromUnit === 'fahrenheit' &&
+      this.selectedToUnit === 'kelvin'
+    ) {
+      this.result = (this.inputTemperature - 32) * (5 / 9) + 273.15;
+    } else if (
+      this.selectedFromUnit === 'kelvin' &&
+      this.selectedToUnit === 'celsius'
+    ) {
+      this.result = this.inputTemperature - 273.15;
+    } else if (
+      this.selectedFromUnit === 'kelvin' &&
+      this.selectedToUnit === 'fahrenheit'
+    ) {
+      this.result = ((this.inputTemperature - 273.15) * 9) / 5 + 32;
+    }
   }
-
 }
